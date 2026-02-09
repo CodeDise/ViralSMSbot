882b1a59ac5ff29949c52822431b892439f8fc31
CODEDISE <otxstudioofficial@gmail.com>
2026-02-09 17:37:50 +0530

    Update shortlink service and tutorial video URLs

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/a50289c5498b927
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/eb60dc67795c52efd5f1b-4600a893c5dc69792e.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "droplink.co")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "d4f7a749e3f9f7bddbba40310992df686bb22d4d")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/How_To_Download_OTx")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "nowshort.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "e3b32859bdb46cc11255422458f8e433d8176911")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/How_To_open_nowshort")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://i.ibb.co/c5GRpjv/x.jpg")
 #--------------------------------------------
